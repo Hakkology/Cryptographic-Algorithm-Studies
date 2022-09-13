05d63a2521c76ade4d4db8c30fb9a9e1ab42ce2e
Hakan Yildiz <107430060+Hakkology@users.noreply.github.com>
2022-09-13 21:36:03 +0300

    Add files via upload

--- CS502-Substitution/Program.cs
+++ CS502-Substitution/Program.cs
@@ -10,7 +10,77 @@ namespace CS502_Substitution
     {
         static void Main(string[] args)
         {
+            string Key = "YTNSHKVEFXRBAUQZCLWDMIPGJO";
 
+            Console.WriteLine("Enter the text to be encoded: ");
+            string Input = Console.ReadLine();
+            Console.WriteLine(Encode(Input, Key));
+            Console.ReadLine();
+            Console.WriteLine(Decode(Encode(Input, Key), Key));
+            Console.ReadLine();
+        }
+
+
+        static string Encode(string input, string key)
+        {
+            char[] charinputcheck=input.ToCharArray();
+            char[] charinput = input.ToUpper().ToCharArray();
+            char[] charkey = key.ToUpper().ToCharArray();
+            char[] charoutput = new char[charinput.Length];
+            //Encoding
+            for (int i = 0; i < charinput.Length; i++)
+            {
+                if (charinput[i] ==' ' || Char.IsDigit(charinput[i]))
+                {
+                    charoutput[i] = charinput[i];
+                }
+                else
+                {
+                    int j = charinput[i] - 65;
+                    charoutput[i] = charkey[j];
+                }
+            }
+            //Case Sensitivity
+            for (int i = 0; i < charoutput.Length; i++)
+            {
+                if (Char.IsLower(charinputcheck[i]))
+                {
+                    charoutput[i] = Char.ToLower(charoutput[i]);
+                }
+            }
+            string Output = new string(charoutput);
+            return Output;
+        }
+
+        static string Decode(string output, string key)
+        {
+            char[] charoutputcheck = output.ToCharArray();
+            char[] charoutput = output.ToUpper().ToCharArray();
+            char[] charkey = key.ToUpper().ToCharArray();
+            char[] charinput = new char[charoutput.Length];
+            //Decoding
+            for (int i = 0; i < charoutput.Length; i++)
+            {
+                if (charoutput[i] == ' ' || Char.IsDigit(charoutput[i]))
+                {
+                    charinput[i] = charoutput[i];
+                }
+                else
+                {
+                    int j = key.IndexOf(charoutput[i]) + 65;
+                    charinput[i] = (char)j;
+                }
+            }
+            //Case Sensitivity
+            for (int i = 0; i < charinput.Length; i++)
+            {
+                if (Char.IsLower(charoutputcheck[i]))
+                {
+                    charinput[i] = Char.ToLower(charinput[i]);
+                }
+            }
+            string Input = new string(charinput);
+            return Input;
         }
     }
 }
---
+++ Hakan_RSAAlgorithm/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Hakan_RSAAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var csp = new RSACryptoServiceProvider();
+            var privateKey = csp.ExportParameters(true);
+            var publicKey= csp.ExportParameters(false);
+
+            string publicKeystring;
+            {
+                var sw = new System.IO.StringWriter();
+                var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+                xs.Serialize(sw, publicKey);
+                publicKeystring=sw.ToString();
+            }
+
+            {
+                var sr = new System.IO.StringReader(publicKeystring);
+                var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+                publicKey = (RSAParameters)xs.Deserialize(sr);
+            }
+
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(publicKey);
+
+            var PlainText = "Run, you fools!";
+            Console.WriteLine(PlainText);
+            Console.ReadLine();
+            var bytesPlainText = System.Text.Encoding.Unicode.GetBytes(PlainText);
+            var bytesCypherText = csp.Encrypt(bytesPlainText, false);
+            var cypherText = Convert.ToBase64String(bytesCypherText);
+            Console.WriteLine(cypherText);
+            Console.ReadLine();
+
+            bytesCypherText = Convert.FromBase64String(cypherText);
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(privateKey);
+            bytesPlainText = csp.Decrypt(bytesCypherText, false);
+            PlainText = System.Text.Encoding.Unicode.GetString(bytesPlainText);
+            Console.WriteLine(PlainText);
+            Console.ReadLine();
+        }
+    }
+}
